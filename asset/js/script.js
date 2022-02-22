a6b42c83f85f4ad5df317f2f9c1f33b696dfa830
Philip <philipmboyah@gmail.com>
2022-02-22 04:38:53 +0000

    changes made

--- asset/js/script.js
+++ asset/js/script.js
@@ -1,19 +1,27 @@
-var buttonEI = document.querySelector("#save-task");
-console.log(buttonEI);
-
-var buttonEI =document.querySelector("#save-task");
+var formEI = document.querySelector("#task-form");
 var tasksToDoEI = document.querySelector("#tasks-to-do");
-buttonEI.addEventListener("click",function(){
+
+var createTaskHandler = function(event) {
+    event.preventDefault();
+    
+    var taskNameInput = document.querySelector("input[name='task-name']");
+    console.log(taskNameInput);
+    
+    var taskTypeInput = document.querySelector("input[name='task-type']").value;
+    //create list item
     var listItemEI = document.createElement("li");
-    listItemEI.className ="task-item";
-    listItemEI.textContent ="This is a new task.";
-    tasksToDoEI.appendChild(listItemEI);
-});
-var createTaskHandler = function(){
-    var listItemEI = document.createElement("li");
-    listItemEI.className ="task-item";
-    listItemEI.textContent ="This is a new task.";
+    listItemEI.className = "task-item";
+
+    //create div to hole task info and add to list item
+    var taskInfoEI = document.createElement("div")
+    taskInfoEI.className="task-info";
+
+    //add HTML content to div
+    taskInfoEI.innerHTML = "<h3 class='task-name'>" + taskNameInput + "</h3><span class='task-type'>" + taskTypeInput + "</span>";
+    listItemEI.appendChild(taskInfoEI);
+
+    //add entire list item to list
     tasksToDoEI.appendChild(listItemEI);
 };
-buttonEI.addEventListener("click",createTaskHandler);
+formEI.addEventListener("submit", createTaskHandler);
 
